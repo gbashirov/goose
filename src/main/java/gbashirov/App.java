3e6f6b64c7026c7de2d87b883a63fd6ee61a1e49
gbashirov <grigory.bashirov@accenture.com>
2019-07-30 22:21:28 +0200

    Init from maven-archetype-simple 1.4

---
+++ src/main/java/gbashirov/App.java
@@ -0,0 +1,13 @@
+package gbashirov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
